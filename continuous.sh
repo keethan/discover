718d9c439062ba50b9b06fab98b6ffab8019b7d6
Heramb Deware <heramb@heramb-Latitude-E6420.(none)>
2013-02-28 21:22:51 +0530

    Fixed android version check. Instead of checking for 4.1.1 now it checks for 4.1.x. Also removed unwanted puts statements and removed time stamping for each step.

--- continuous.sh
+++ continuous.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 x=1
-while [ $x -le 5000 ]
+while [ $x -le 5 ]
 do
 echo "Execution: $x"
 	
-TEST_SERVER_PORT="34777" ADB_DEVICE_ARG="42f14a1c1ed57f75" calabash-android run adw_new_debug.apk
+#TEST_SERVER_PORT="34777" ADB_DEVICE_ARG="42f7b50fb91b9fcf" calabash-android run adw_new_debug.apk
 
-sleep 20
+#sleep 20
 
-TEST_SERVER_PORT="34778" ADB_DEVICE_ARG="HT255TV02934" calabash-android run adw_new_debug.apk
+TEST_SERVER_PORT="34778" ADB_DEVICE_ARG="HT255TV02934" calabash-android run adw_new_debug.apk features/my_first.feature
 
-sleep 20
-
-TEST_SERVER_PORT="34779" ADB_DEVICE_ARG="SH28EW502803" calabash-android run adw_new_debug.apk
-
-sleep 20
+#sleep 20
 	
 x=$(( $x + 1 ))
 done
